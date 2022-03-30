275a7fe8689376159acd2eb8ac776f88e0904032
Anjaly1999 <74658788+Anjaly1999@users.noreply.github.com>
2022-03-30 12:25:22 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 30 15:42:30 2022
+
+@author: sjcet
+"""
+
+import numpy as np
+x=np.array([[1,2,8],[5,6,9]])
+y=np.array([[3,4,7],[7,8,10]])
+print("Add 2 matrices",np.add(x,y))
+print("Substract 2 matrices",np.subtract(x,y))
